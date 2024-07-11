ad90e362a86bf05f1dae8b08e244b5daaee2ae4d
Nguyen Pham <nguyen.pham@neurmeka.com>
2024-07-11 17:15:04 +0700

    update indyeye, fix moby client msg issue, add requires range

--- python/neuromeka/moby.py
+++ python/neuromeka/moby.py
@@ -3,8 +3,8 @@ from neuromeka.common import *
 
 import grpc, time
 
-
 MOBY_PORT = 20200
+
 
 class MobyClient:
     def __init__(self, ip_addr):
@@ -32,7 +32,7 @@ class MobyClient:
             7: "MOVING",
             16: "TELE_OP",
         }
-        state = self.__moby_stub.GetMobyState(Empty())
+        state = self.__moby_stub.GetMobyState(common_msgs.Empty())
         moby_state = state_dict.get(state.status, "(UNKNOWN STATE CODE)")
         b_ready = state.is_ready
         b_enable = state.is_enable
@@ -58,21 +58,21 @@ class MobyClient:
             0x100: "HW_CONNECTION_LOST",
         }
 
-        err = self.__moby_stub.GetMobyErrorState(Empty())
+        err = self.__moby_stub.GetMobyErrorState(common_msgs.Empty())
 
         return [error_dict.get(err.errorState, "(UNKNOWN ERROR CODE)"),
                 err.errorIndex1, err.errorIndex2, err.errorIndex3]
 
     @Utils.exception_handler
     def recover(self):
-        return self.__moby_stub.Recover(Empty())
+        return self.__moby_stub.Recover(common_msgs.Empty())
 
     @Utils.exception_handler
     def get_moby_pose(self):
         """
         Get Moby pose (m): [Px, Py, Pw]
         """
-        pose = self.__moby_stub.GetMobyPose(Empty())
+        pose = self.__moby_stub.GetMobyPose(common_msgs.Empty())
         return [pose.px, pose.py, pose.pw]
 
     @Utils.exception_handler
@@ -80,7 +80,7 @@ class MobyClient:
         """
         Get Moby velocity (m/s): [Vx, Vy, Vw]
         """
-        vel = self.__moby_stub.GetMobyVel(Empty())
+        vel = self.__moby_stub.GetMobyVel(common_msgs.Empty())
         return [vel.vx, vel.vy, vel.vw]
 
     @Utils.exception_handler
@@ -88,30 +88,30 @@ class MobyClient:
         """
         Reset Moby pose
         """
-        return self.__moby_stub.ResetMobyPose(Empty())
+        return self.__moby_stub.ResetMobyPose(common_msgs.Empty())
 
     @Utils.exception_handler
     def get_rotation_angle(self):
         """
         Get rotation angle (deg): [fl, fr, bl, br]
         """
-        val = self.__moby_stub.GetRotationAngleDeg(Empty())
-        return {'fl': val.fl, 'fr': val.fr, 'bl':val.bl, 'br':val.br}
+        val = self.__moby_stub.GetRotationAngleDeg(common_msgs.Empty())
+        return {'fl': val.fl, 'fr': val.fr, 'bl': val.bl, 'br': val.br}
 
     @Utils.exception_handler
     def get_drive_speed(self):
         """
         Get drive speed (m/s): [fl, fr, bl, br]
         """
-        val = self.__moby_stub.GetDriveSpeed(Empty())
-        return {'fl': val.fl, 'fr': val.fr, 'bl':val.bl, 'br':val.br}
+        val = self.__moby_stub.GetDriveSpeed(common_msgs.Empty())
+        return {'fl': val.fl, 'fr': val.fr, 'bl': val.bl, 'br': val.br}
 
     @Utils.exception_handler
     def get_target_vel(self):
         """
         Get Moby's target velocity
         """
-        target = self.__moby_stub.GetTargetVel(Empty())
+        target = self.__moby_stub.GetTargetVel(common_msgs.Empty())
         return [target.vx, target.vy, target.vw]
 
     @Utils.exception_handler
@@ -120,22 +120,22 @@ class MobyClient:
         Get rotation's zero position (encoder count)
                                     [fl, fr, bl, br]
         """
-        val = self.__moby_stub.GetRotationZeroCount(Empty())
-        return {'fl': val.fl, 'fr': val.fr, 'bl':val.bl, 'br':val.br}
+        val = self.__moby_stub.GetRotationZeroCount(common_msgs.Empty())
+        return {'fl': val.fl, 'fr': val.fr, 'bl': val.bl, 'br': val.br}
 
     @Utils.exception_handler
     def get_gyro_data(self):
         """
         Get Gyro sensor data (yaw, yaw rate)
         """
-        return self.__moby_stub.GetGyroData(Empty()).val
+        return self.__moby_stub.GetGyroData(common_msgs.Empty()).val
 
     @Utils.exception_handler
     def get_imu_data(self):
         """
         Get Full IMU sensor data
         """
-        data = self.__moby_stub.GetGyroFullData(Empty())
+        data = self.__moby_stub.GetGyroFullData(common_msgs.Empty())
         angle = [data.angleX, data.angleY, data.angleZ]
         vel = [data.angleVelX, data.angleVelY, data.angleVelZ]
         acc = [data.linAccX, data.linAccY, data.linAccZ]
@@ -146,23 +146,29 @@ class MobyClient:
         """
         Reset gyro sensor
         """
-        return self.__moby_stub.ResetGyroSensor(Empty())
+        return self.__moby_stub.ResetGyroSensor(common_msgs.Empty())
 
     @Utils.exception_handler
     def use_gyro_for_odom(self, use_gyro):
         """
         Use gyro sensor for odometry calculation
         """
-        return self.__moby_stub.UseGyroForOdom(BoolVal(val=use_gyro))
+        return self.__moby_stub.UseGyroForOdom(moby_msgs.BoolVal(val=use_gyro))
 
     @Utils.exception_handler
     def get_us_data(self):
         """
         Get US sensor data
         """
-        value = self.__moby_stub.GetUSSensorData(Empty())
-        return {'front_left1': value.us_front_left1, 'front_left2': value.us_front_left2, 'front_left3': value.us_front_left3, 'front_ground': value.us_front_ground, 'front_right1': value.us_front_right1, 'front_right2': value.us_front_right2, 'front_right3': value.us_front_right3, 'front_right4': value.us_front_right4, 'back_right1': value.us_back_right1, 'back_right2': value.us_back_right2, 'back_right3': value.us_back_right3, 'back_ground': value.us_back_ground, 'back_left1': value.us_back_left1, 'back_left2': value.us_back_left2, 'back_left3': value.us_back_left3, 'back_left4': value.us_back_left4}
-    
+        value = self.__moby_stub.GetUSSensorData(common_msgs.Empty())
+        return {'front_left1': value.us_front_left1, 'front_left2': value.us_front_left2,
+                'front_left3': value.us_front_left3, 'front_ground': value.us_front_ground,
+                'front_right1': value.us_front_right1, 'front_right2': value.us_front_right2,
+                'front_right3': value.us_front_right3, 'front_right4': value.us_front_right4,
+                'back_right1': value.us_back_right1, 'back_right2': value.us_back_right2,
+                'back_right3': value.us_back_right3, 'back_ground': value.us_back_ground,
+                'back_left1': value.us_back_left1, 'back_left2': value.us_back_left2, 'back_left3': value.us_back_left3,
+                'back_left4': value.us_back_left4}
 
     @Utils.exception_handler
     def get_bms(self):
@@ -184,7 +190,7 @@ class MobyClient:
             'Remain Capacity Ah', 'Remain Capacity Wh'
             'Temperature-(1~3)', 'Cell Voltage-(1~13)'
         """
-        value = self.__moby_stub.GetBMSData(Empty())
+        value = self.__moby_stub.GetBMSData(common_msgs.Empty())
         return {'BMS status-1': value.bms_status[0] / 10, 'BMS status-2': value.bms_status[1] / 10,
                 'Pack voltage-1': value.pack_volt[0] / 100, 'Pack voltage-2': value.pack_volt[1] / 100,
                 'Battery Voltage-1': value.battery_volt[0] / 100, 'Battery Voltage-2': value.battery_volt[1] / 100,
@@ -211,35 +217,35 @@ class MobyClient:
         """
         Drive control
         """
-        return self.__moby_stub.SetStepControl(TargetVel(vx=vx, vy=vy, vw=vw))
+        return self.__moby_stub.SetStepControl(moby_msgs.TargetVel(vx=vx, vy=vy, vw=vw))
 
     @Utils.exception_handler
     def stop_motion(self):
         """
         Stop Moby motion
         """
-        return self.__moby_stub.StopMotion(Empty())
+        return self.__moby_stub.StopMotion(common_msgs.Empty())
 
     @Utils.exception_handler
     def go_straight(self):
         """
         Go straight (zero rotation)
         """
-        return self.__moby_stub.SetRotationAngleDeg(SwerveDoubles(fl=0, fr=0, bl=0, br=0))
+        return self.__moby_stub.SetRotationAngleDeg(moby_msgs.SwerveDoubles(fl=0, fr=0, bl=0, br=0))
 
     @Utils.exception_handler
     def move_rotation_deg(self, fl, fr, bl, br):
         """
         Rotation control (target angle degree)
         """
-        return self.__moby_stub.SetRotationAngleDeg(SwerveDoubles(fr=fr, br=br, bl=bl, fl=fl))
+        return self.__moby_stub.SetRotationAngleDeg(moby_msgs.SwerveDoubles(fr=fr, br=br, bl=bl, fl=fl))
 
     @Utils.exception_handler
     def move_driving_mps(self, fl, fr, bl, br):
         """
         Driving control (target speed m/s)
         """
-        return self.__moby_stub.DriveWheel(SwerveDoubles(fr=fr, br=br, bl=bl, fl=fl))
+        return self.__moby_stub.DriveWheel(moby_msgs.SwerveDoubles(fr=fr, br=br, bl=bl, fl=fl))
 
     ############################
     # Set Moby parameters
@@ -250,14 +256,14 @@ class MobyClient:
         """
         Set current roation position as zero
         """
-        return self.__moby_stub.SetZeroPosAsCurrentPos(Empty())
+        return self.__moby_stub.SetZeroPosAsCurrentPos(common_msgs.Empty())
 
     @Utils.exception_handler
     def set_rotation_vel_acc(self, vel, acc):
         """
         Set rotation maximum velocity, acceleration
         """
-        return self.__moby_stub.SetRotationVelAcc(DoubleVals(val=[vel, acc]))
+        return self.__moby_stub.SetRotationVelAcc(moby_msgs.DoubleVals(val=[vel, acc]))
 
     @Utils.exception_handler
     def set_rotation_interpolator(self, val):
@@ -268,21 +274,21 @@ class MobyClient:
         2: Velocity interpolator
         3: Trapezoidal interpolator
         """
-        return self.__moby_stub.SetRotationInterpolator(IntVal(val=val))
+        return self.__moby_stub.SetRotationInterpolator(moby_msgs.IntVal(val=val))
 
     @Utils.exception_handler
     def set_drive_acc_dec(self, acc, dec):
         """
         Set drive acc dec
         """
-        return self.__moby_stub.SetDriveAccDec(DoubleVals(val=[acc, dec]))
+        return self.__moby_stub.SetDriveAccDec(moby_msgs.DoubleVals(val=[acc, dec]))
 
     @Utils.exception_handler
     def set_drive_interpolator_on_off(self, on):
         """
         Set drive interpolator On Off
         """
-        return self.__moby_stub.SetDriveInterpolatorOnOff(BoolVal(val=on))
+        return self.__moby_stub.SetDriveInterpolatorOnOff(moby_msgs.BoolVal(val=on))
 
     @Utils.exception_handler
     def set_rotation_controller_type(self, val):
@@ -292,69 +298,68 @@ class MobyClient:
         1: SIMPLEPID_POS_CONTROLLER,
         2: SIMPLEPID_VEL_CONTROLLER
         """
-        return self.__moby_stub.SetRotationControllerType(IntVal(val=val))
+        return self.__moby_stub.SetRotationControllerType(moby_msgs.IntVal(val=val))
 
     @Utils.exception_handler
     def set_rotation_gain(self, index, k, kv, kp):
         """
         Set Rotation Control Gain
         """
-        return self.__moby_stub.SetControlParam(RotationGain(idx=index, k=k, kv=kv, kp=kp))
+        return self.__moby_stub.SetControlParam(moby_msgs.RotationGain(idx=index, k=k, kv=kv, kp=kp))
 
     @Utils.exception_handler
     def get_rotation_gain(self, index):
         """
         Get Rotation Control Gain (k, kv, kp)
         """
-        val = self.__moby_stub.GetControlParam(IntVal(val=index))
-        return {'k': val.k, 'kv':val.kv, 'kp':val.kp}
-    
+        val = self.__moby_stub.GetControlParam(moby_msgs.IntVal(val=index))
+        return {'k': val.k, 'kv': val.kv, 'kp': val.kp}
+
     @Utils.exception_handler
     def set_kinematics_forced(self, onoff):
         """
         Set Kinematics Forced( on=true, off=false )
         """
-        self.__moby_stub.SetForceKinematics(BoolVal(val=onoff))
+        self.__moby_stub.SetForceKinematics(moby_msgs.BoolVal(val=onoff))
 
     @Utils.exception_handler
     def get_kinematics_forced(self):
         """
         Get Kinematics Forced( on=true, off=false )
         """
-        val = self.__moby_stub.GetForceKinematics(Empty())
+        val = self.__moby_stub.GetForceKinematics(common_msgs.Empty())
         return val
 
     ############################
     # Moby-Agri related commands
     ############################
-        
+
     @Utils.exception_handler
     def turn_light(self, on):
-        return self.__moby_stub.TurnLightOnOff(BoolVal(val=on))
-        
+        return self.__moby_stub.TurnLightOnOff(moby_msgs.BoolVal(val=on))
+
     @Utils.exception_handler
     def turn_buzzer(self, on):
-        return self.__moby_stub.TurnBuzzOnOff(BoolVal(val=on))
+        return self.__moby_stub.TurnBuzzOnOff(moby_msgs.BoolVal(val=on))
 
     @Utils.exception_handler
     def pause_bumper(self, on):
-        return self.__moby_stub.PauseBumper(BoolVal(val=on))
+        return self.__moby_stub.PauseBumper(moby_msgs.BoolVal(val=on))
 
     ############################
     # Moby Data logging
     ############################
-    
+
     @Utils.exception_handler
     def start_rt_logging(self):
         """
         Start RT logging
         """
-        return self.__moby_stub.StartRTLogging(Empty())
-    
+        return self.__moby_stub.StartRTLogging(common_msgs.Empty())
+
     @Utils.exception_handler
     def end_rt_logging(self):
         """
         End RT logging
         """
-        return self.__moby_stub.EndRTLogging(Empty())
-
+        return self.__moby_stub.EndRTLogging(common_msgs.Empty())
